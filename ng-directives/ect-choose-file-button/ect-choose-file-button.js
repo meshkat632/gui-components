2fa252ed83265eb9b25bbe3459c6ba8709b4208c
meshkat <meshkat632@gmail.com>
2015-10-05 10:34:19 +0200

    [asdasdDDD]

--- ng-directives/ect-choose-file-button/ect-choose-file-button.js
+++ ng-directives/ect-choose-file-button/ect-choose-file-button.js
@@ -2,17 +2,13 @@ angular.module('ect.directives.ectChooseFileButton', []).directive('ectChooseFil
 	var counter = 0;
     return {
 		templateUrl : 'ect-choose-file-button.html',
-		//replace : true,
         transclude: true,
 		scope : {
             multiple:"@",
             'onSelect': '&onSelect'
 		},
-		controller : function($scope) {
-			console.log('ectChooseFileButton controller');
-		},
 		link : function($scope, element, attr) {
-			console.log('ectChooseFileButton multiple', attr.multiple);
+			//console.log('ectChooseFileButton multiple', attr.multiple);
             counter = counter+1;
             $scope.id ='ectChooseFileButton_'+counter;
             element.attr('id',$scope.id);
@@ -22,24 +18,19 @@ angular.module('ect.directives.ectChooseFileButton', []).directive('ectChooseFil
                 $scope.multiple = '';
             else{
                 element.find('.sendFile').attr('multiple','multiple');
-                $scope.multiple = 'multiple';
-            }
+                $scope.multiple = 'multiple';            }
                 var $sendFile = element.find('.sendFile');
                 console.log('$sendFile:',$sendFile);
                 $sendFile.bind('click', function() {
-                    console.log('on click');
                     var sendFile = $sendFile.get(0);
                     sendFile.onchange = function(e) {
-                        console.log('onchange', sendFile.files);
                         var files = [];
                         var keys = Object.keys(sendFile.files);
                         keys.forEach(function(key){
                             var file = sendFile.files[key];
-                            console.log('file', file);
-                            //files.push(angular.copy(file));
                             files.push(file);
                         });
-                        $scope.onSelect({files:files});
+                        $scope.onSelect({files:files, id:$scope.id});
                     };
                 });
             console.log('ectChooseFileButton:', $scope.id, $scope.multiple, $scope.onSelect);
