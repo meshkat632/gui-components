2de095b741d7067ac4e6b6e8d6852d4d61765c1c
meshkat <meshkat632@gmail.com>
2015-10-05 15:38:40 +0200

    sadsad

--- ect-choose-file-button/ect-choose-file-button.js
+++ ect-choose-file-button/ect-choose-file-button.js
@@ -1,7 +1,62 @@
-angular.module('ect.directives.ectChooseFileButton', []).directive('ectChooseFileButton', ['$document', '$timeout', '$interval', function ($document, $timeout, $interval) {
+angular.module('ect.directives.ectChooseFileButton', []).directive('ectChooseFileButton', ['$document', '$timeout', '$interval','$templateCache', function ($document, $timeout, $interval, $templateCache) {
+
+    var strVar="";
+    strVar += "<span id={{id}}>";
+    strVar += "<style>";
+    strVar += "";
+    strVar += "    .btn-file {";
+    strVar += "        position: relative;";
+    strVar += "        overflow: hidden;";
+    strVar += "    }";
+    strVar += "    .btn-file input[type=file] {";
+    strVar += "";
+    strVar += "        position: absolute;";
+    strVar += "        top: 0px;";
+    strVar += "        right: 0px;";
+    strVar += "";
+    strVar += "        min-width: 100%;";
+    strVar += "        min-height: 100%;";
+    strVar += "        opacity: 0;";
+    strVar += "        outline: none;";
+    strVar += "        cursor: inherit;";
+    strVar += "        display: block;";
+    strVar += "    }";
+    strVar += "";
+    strVar += "";
+    strVar += "    \/*";
+    strVar += "    #{{id}}{";
+    strVar += "";
+    strVar += "    }";
+    strVar += "    #{{id}} button {";
+    strVar += "        position: relative;";
+    strVar += "        overflow: hidden;";
+    strVar += "    }";
+    strVar += "    #{{id}} button input[type=file] {";
+    strVar += "";
+    strVar += "        position: absolute;";
+    strVar += "        top: 0px;";
+    strVar += "        right: 0px;";
+    strVar += "";
+    strVar += "        min-width: 100%;";
+    strVar += "        min-height: 100%;";
+    strVar += "        opacity: 0;";
+    strVar += "        outline: none;";
+    strVar += "        cursor: inherit;";
+    strVar += "        display: block;";
+    strVar += "    }";
+    strVar += "    *\/";
+    strVar += "<\/style>";
+    strVar += "    <button type=\"file\" class=\"btn btn-info btn-sm btn-file\">";
+    strVar += "        <ng-transclude><\/ng-transclude>";
+    strVar += "        <input class='sendFile' type=\"file\" >";
+    strVar += "    <\/button>";
+    strVar += "<\/span>";
+
     var counter = 0;
+    //console.log('templateUrl:',templateUrlMapper['ect-choose-file-button']);
     return {
-        templateUrl: 'ect-choose-file-button.html',
+        //templateUrl: window.templateUrlMapper['ect-choose-file-button'],//'ect-choose-file-button.html',
+        template :strVar,
         transclude: true,
         scope: {
             multiple: "@",
